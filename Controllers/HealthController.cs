0a9be26e0676d0e70539681e4d2173d84a36e56a
Mario Godoy <godoymario110@gmail.com>
2025-08-09 10:39:28 -0600

    Initial enviroment

---
+++ Controllers/HealthController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("healt")]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new { status = "OK", timestamp = System.DateTime.UtcNow });
+        }
+    }
+}
